a464f7ab70ff31918e7aa3510f27cf1d0505a205
Olga-Zyukina <bloger_80@mail.ru>
2026-02-13 10:25:45 +0300

    del link from footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@
       &copy; Copyright <strong><span>CryptoVue</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
-      Designed by <a href="/">CryptoVue</a>
+      Designed by CryptoVue
     </div>
   </footer>
 </template>
