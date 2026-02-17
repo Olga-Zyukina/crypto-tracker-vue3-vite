1b0238edbae5c4b2fcd4258e3024ba34aff7f35d
Olga-Zyukina <bloger_80@mail.ru>
2026-02-17 10:44:25 +0300

    udate breadcrumbs

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,7 +10,11 @@
       </h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="/">Home</a></li>
+         <li class="breadcrumb-item">
+            <RouterLink :to="{ name: 'home' }">
+              Home
+            </RouterLink>
+          </li>
           <li class="breadcrumb-item active">
             <slot name="title"> </slot>
           </li>
